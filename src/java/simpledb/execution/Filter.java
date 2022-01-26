b51a7fc8b45f2025f3e0a74822d42e4df24fa2d8
anarion <anarionzuo@outlook.com>
2022-01-26 12:05:02 +0800

    lab2 exercise 1 filter joinpredicate tests passed

--- src/java/simpledb/execution/Filter.java
+++ src/java/simpledb/execution/Filter.java
@@ -14,6 +14,9 @@ public class Filter extends Operator {
 
     private static final long serialVersionUID = 1L;
 
+    private final Predicate predicate;
+    private OpIterator child;
+
     /**
      * Constructor accepts a predicate to apply and a child operator to read
      * tuples to filter from.
@@ -25,29 +28,37 @@ public class Filter extends Operator {
      */
     public Filter(Predicate p, OpIterator child) {
         // some code goes here
+        predicate = p;
+        this.child = child;
     }
 
     public Predicate getPredicate() {
         // some code goes here
-        return null;
+        return predicate;
     }
 
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
+        return child.getTupleDesc();
     }
 
     public void open() throws DbException, NoSuchElementException,
             TransactionAbortedException {
         // some code goes here
+        child.open();
+        rewind();
+        super.open();
     }
 
     public void close() {
         // some code goes here
+        super.close();
+        child.close();
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
         // some code goes here
+        child.rewind();
     }
 
     /**
@@ -62,18 +73,29 @@ public class Filter extends Operator {
     protected Tuple fetchNext() throws NoSuchElementException,
             TransactionAbortedException, DbException {
         // some code goes here
+        while (child.hasNext()) {
+            Tuple tuple = child.next();
+            if (predicate.filter(tuple)) {
+                return tuple;
+            }
+        }
+        // If code reaches here, the child iterator must contain invalid tuple.
+        // Or it would have returned by filter check.
         return null;
     }
 
     @Override
     public OpIterator[] getChildren() {
         // some code goes here
-        return null;
+        return new OpIterator[] { child };
     }
 
     @Override
     public void setChildren(OpIterator[] children) {
         // some code goes here
+        if (child != children[0]) {
+            child = children[0];
+        }
     }
 
 }
--- src/java/simpledb/execution/JoinPredicate.java
+++ src/java/simpledb/execution/JoinPredicate.java
@@ -1,6 +1,5 @@
 package simpledb.execution;
 
-import simpledb.storage.Field;
 import simpledb.storage.Tuple;
 
 import java.io.Serializable;
@@ -12,6 +11,11 @@ import java.io.Serializable;
 public class JoinPredicate implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    private final int field1;
+    private final int field2;
+
+    private final Predicate.Op op;
 
     /**
      * Constructor -- create a new predicate over two fields of two tuples.
@@ -29,6 +33,9 @@ public class JoinPredicate implements Serializable {
      */
     public JoinPredicate(int field1, Predicate.Op op, int field2) {
         // some code goes here
+        this.field1 = field1;
+        this.field2 = field2;
+        this.op = op;
     }
 
     /**
@@ -39,24 +46,24 @@ public class JoinPredicate implements Serializable {
      */
     public boolean filter(Tuple t1, Tuple t2) {
         // some code goes here
-        return false;
+        return t1.getField(field1).compare(op, t2.getField(field2));
     }
     
     public int getField1()
     {
         // some code goes here
-        return -1;
+        return field1;
     }
     
     public int getField2()
     {
         // some code goes here
-        return -1;
+        return field2;
     }
     
     public Predicate.Op getOperator()
     {
         // some code goes here
-        return null;
+        return op;
     }
 }
