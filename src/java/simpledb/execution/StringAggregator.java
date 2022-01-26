580ad93a70d3bf578e62d1cc7bf492636b4c517a
anarion <anarionzuo@outlook.com>
2022-01-26 22:13:36 +0800

    lab2 exercise 2 StringAggregator tests passed

--- src/java/simpledb/execution/StringAggregator.java
+++ src/java/simpledb/execution/StringAggregator.java
@@ -1,12 +1,16 @@
 package simpledb.execution;
 
+import simpledb.common.DbException;
 import simpledb.common.Type;
-import simpledb.storage.Tuple;
+import simpledb.storage.*;
+import simpledb.transaction.TransactionAbortedException;
+
+import java.util.*;
 
 /**
  * Knows how to compute some aggregate over a set of StringFields.
  */
-public class StringAggregator implements Aggregator {
+public class StringAggregator extends Aggregator {
 
     private static final long serialVersionUID = 1L;
 
@@ -21,14 +25,125 @@ public class StringAggregator implements Aggregator {
 
     public StringAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
         // some code goes here
+        super(gbfield, gbfieldtype, afield, what);
+    }
+
+    private String getTupleAggregateVal(Tuple tuple) {
+        return ((StringField) tuple.getField(aggregateField)).getValue();
+    }
+
+    private class TupleComparator implements Comparator<Tuple> {
+        @Override
+        public int compare(Tuple x, Tuple y) {
+            return getTupleAggregateVal(x).compareTo(getTupleAggregateVal(y));
+        }
     }
 
     /**
-     * Merge a new tuple into the aggregate, grouping as indicated in the constructor
-     * @param tup the Tuple containing an aggregate field and a group-by field
+     * Executes aggregate schemes.
+     * @param tupleList List of tuples to be aggregated.
+     * @return result of aggregation
      */
-    public void mergeTupleIntoGroup(Tuple tup) {
-        // some code goes here
+    private int doAggregate(ArrayList<Tuple> tupleList) {
+        switch (operation) {
+            /*case MIN:
+                return getTupleAggregateVal(tupleList.stream().min(new TupleComparator()).get());
+            case MAX:
+                return getTupleAggregateVal(tupleList.stream().max(new TupleComparator()).get());
+            case SUM:
+                int ret = 0;
+                for (var tuple : tupleList) {
+                    ret += getTupleAggregateVal(tuple);
+                }
+                return ret;*/
+            case COUNT:
+                return tupleList.size();
+            /*case AVG:
+                ret = 0;
+                for (var tuple : tupleList) {
+                    ret += getTupleAggregateVal(tuple);
+                }
+                return ret / tupleList.size();*/
+            default:
+//                throw new ExecutionControl.NotImplementedException("aggregate operation not implemented");
+        }
+        return -1;
+    }
+
+    private class AggregatorOperator implements OpIterator {
+
+        Iterator<Map.Entry<Field, ArrayList<Tuple>>> mapIterator;
+        boolean noGroupAccessed = false;
+        boolean opened = false;
+
+        public AggregatorOperator() {
+            initEntryIterator();
+        }
+
+        private void initEntryIterator() {
+            mapIterator = groupMap.entrySet().iterator();
+            noGroupAccessed = false;
+        }
+
+        @Override
+        public void open() throws DbException, TransactionAbortedException {
+            opened = true;
+            initEntryIterator();
+        }
+
+        @Override
+        public boolean hasNext() throws DbException, TransactionAbortedException {
+            if (!this.opened)
+                throw new IllegalStateException("Operator not yet open");
+            if (hasNoGroupBy()) {
+                return !noGroupAccessed;
+            }
+            return mapIterator.hasNext();
+        }
+
+        @Override
+        public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException {
+            if (!this.opened)
+                throw new IllegalStateException("Operator not yet open");
+            if (hasNoGroupBy()) {
+                noGroupAccessed = true;
+                return new Tuple(getTupleDesc(), new Field[] { new IntField(doAggregate(nogroupList))});
+            } else {
+                var entry = mapIterator.next();
+                var groupVal = entry.getKey();
+                var groupTuple = entry.getValue();
+                return new Tuple(getTupleDesc(), new Field[] {
+                        groupVal,
+                        new IntField(doAggregate(groupTuple))
+                });
+            }
+        }
+
+        @Override
+        public void rewind() throws DbException, TransactionAbortedException {
+            if (!this.opened)
+                throw new IllegalStateException("Operator not yet open");
+            initEntryIterator();
+        }
+
+        @Override
+        public TupleDesc getTupleDesc() {
+            if (hasNoGroupBy()) {
+                return new TupleDesc(new Type[] {Type.INT_TYPE});
+            } else {
+                return new TupleDesc(
+                        new Type[] {groupByFieldType, Type.INT_TYPE}
+                        // does not provide names for fields
+                );
+            }
+        }
+
+        @Override
+        public void close() {
+            if (!this.opened)
+                throw new IllegalStateException("Operator not yet open");
+            opened = false;
+        }
     }
 
     /**
@@ -41,7 +156,7 @@ public class StringAggregator implements Aggregator {
      */
     public OpIterator iterator() {
         // some code goes here
-        throw new UnsupportedOperationException("please implement me for lab2");
+        return new AggregatorOperator();
     }
 
 }
