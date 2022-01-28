4dc9582501c53a599e386ec874f512c7957933ac
anarion <anarionzuo@outlook.com>
2022-01-28 15:29:45 +0800

    lab2 exercise 4 tests passed

--- src/java/simpledb/execution/Delete.java
+++ src/java/simpledb/execution/Delete.java
@@ -3,10 +3,7 @@ package simpledb.execution;
 import simpledb.common.Database;
 import simpledb.common.DbException;
 import simpledb.common.Type;
-import simpledb.storage.BufferPool;
-import simpledb.storage.IntField;
-import simpledb.storage.Tuple;
-import simpledb.storage.TupleDesc;
+import simpledb.storage.*;
 import simpledb.transaction.TransactionAbortedException;
 import simpledb.transaction.TransactionId;
 
@@ -20,6 +17,9 @@ public class Delete extends Operator {
 
     private static final long serialVersionUID = 1L;
 
+    private final TransactionId transactionId;
+    private OpIterator child;
+
     /**
      * Constructor specifying the transaction that this delete belongs to as
      * well as the child to read from.
@@ -31,23 +31,42 @@ public class Delete extends Operator {
      */
     public Delete(TransactionId t, OpIterator child) {
         // some code goes here
+        this.transactionId = t;
+        this.child = child;
     }
 
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
+        return new TupleDesc(
+                new Type[] {Type.INT_TYPE},
+                new String[] {"#deleted"}
+        );
     }
+
+    /**
+     * Record whether this iterator has run through and not been rewound.
+     * Must not directly call child.hasNext() in fetchNext() to check this.
+     * Child may be empty (hasNext returns false) while this iterator must
+     * return a tuple valued 0 as the result of a delete operation.
+     */
+    private boolean hasRunThrough = false;
 
     public void open() throws DbException, TransactionAbortedException {
         // some code goes here
+        child.open();
+        super.open();
     }
 
     public void close() {
         // some code goes here
+        super.close();
+        child.close();
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
         // some code goes here
+        child.rewind();
+        hasRunThrough = false;
     }
 
     /**
@@ -61,18 +80,33 @@ public class Delete extends Operator {
      */
     protected Tuple fetchNext() throws TransactionAbortedException, DbException {
         // some code goes here
-        return null;
+        if (hasRunThrough) {
+            return null;
+        }
+        int count = 0;
+        while (child.hasNext()) {
+            Tuple tuple = child.next();
+            try {
+                Database.getBufferPool().deleteTuple(transactionId, tuple);
+            } catch (IOException e) {
+                System.err.println("delete tuple io failed");
+            }
+            ++count;
+        }
+        hasRunThrough = true;
+        return new Tuple(getTupleDesc(), new Field[] {new IntField(count)});
     }
 
     @Override
     public OpIterator[] getChildren() {
         // some code goes here
-        return null;
+        return new OpIterator[] {child};
     }
 
     @Override
     public void setChildren(OpIterator[] children) {
         // some code goes here
+        child = children[0];
     }
 
 }
